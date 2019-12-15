3d288c77ac821a3453acf74143bda8aa4e6c9f44
AlexxGit <ale-sp@ukr.net>
2019-12-15 13:23:57 +0200

    Print results

--- src/main/java/ru/javaops/masterjava/matrix/MainMatrix.java
+++ src/main/java/ru/javaops/masterjava/matrix/MainMatrix.java
@@ -5,14 +5,18 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class MainMatrix {
-    private static final int MATRIX_SIZE = 1000;
+    private static final int MATRIX_SIZE = 2;
     private static final int THREAD_NUMBER = 10;
 
     private final static ExecutorService executor = Executors.newFixedThreadPool(MainMatrix.THREAD_NUMBER);
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         final int[][] matrixA = MatrixUtil.create(MATRIX_SIZE);
+        System.out.println("matrixA: ");
+        printMatrix(matrixA);
         final int[][] matrixB = MatrixUtil.create(MATRIX_SIZE);
+        System.out.println("matrixB: ");
+        printMatrix(matrixB);
 
         double singleThreadSum = 0.;
         double concurrentThreadSum = 0.;
@@ -21,6 +25,8 @@ public class MainMatrix {
             System.out.println("Pass " + count);
             long start = System.currentTimeMillis();
             final int[][] matrixC = MatrixUtil.singleThreadMultiply(matrixA, matrixB);
+            System.out.println("single thread matrixC:");
+            printMatrix(matrixC);
 
             double duration = (System.currentTimeMillis() - start) / 1000.;
             out("Single thread time, sec: %.3f", duration);
@@ -28,6 +34,8 @@ public class MainMatrix {
 
             start = System.currentTimeMillis();
             final int[][] concurrentMatrixC = MatrixUtil.concurrentMultiply(matrixA, matrixB, executor);
+            System.out.println("concurrent matrixC:");
+            printMatrix(concurrentMatrixC);
 
             duration = (System.currentTimeMillis() - start) / 1000.;
             out("Concurrent thread time, sec: %.3f", duration);
@@ -46,5 +54,15 @@ public class MainMatrix {
 
     private static void out(String format, double ms) {
         System.out.println(String.format(format, ms));
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        int matrixSize = matrix.length;
+        for (int i = 0; i < matrixSize; i++) {
+            for (int j = 0; j < matrixSize; j++) {
+                System.out.print(matrix[i][j] + "\t");
+            }
+            System.out.println();
+        }
     }
 }
--- src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
+++ src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
@@ -1,22 +1,22 @@
 package ru.javaops.masterjava.matrix;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
+import java.util.concurrent.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class MatrixUtil {
 
-    public static int[][] concurrentMultiply(int[][] matrixA, int[][] matrixB, ExecutorService executor) throws InterruptedException, ExecutionException {
+    public static int[][] concurrentMultiplyWithIntStream(int[][] matrixA, int[][] matrixB, ExecutorService executor) throws InterruptedException, ExecutionException {
         final int matrixSize = getLength(matrixA);
         final int[][] matrixC = new int[matrixSize][matrixSize];
 
         List<Callable<Void>> tasks = IntStream.range(0, matrixSize)
                 .mapToObj(j -> new Callable<Void>() {
                     int[] thatColumn = new int[matrixSize];
+
                     @Override
                     public Void call() {
                         for (int k = 0; k < matrixSize; k++) {
@@ -35,6 +35,63 @@ public class MatrixUtil {
                 })
                 .collect(Collectors.toList());
         executor.invokeAll(tasks);
+        return matrixC;
+    }
+
+    public static int[][] concurrentMultiply2(int[][] matrixA, int[][] matrixB, ExecutorService executor) throws InterruptedException, ExecutionException {
+        final int matrixSize = matrixA.length;
+        final int[][] matrixC = new int[matrixSize][];
+
+        final int[][] matrixBT = new int[matrixSize][matrixSize];
+        for (int i = 0; i < matrixSize; i++) {
+            for (int j = 0; j < matrixSize; j++) {
+                matrixBT[i][j] = matrixB[j][i];
+            }
+        }
+
+        List<Callable<Void>> tasks = new ArrayList<>(matrixSize);
+        for (int j = 0; j < matrixSize; j++) {
+            final int row = j;
+            tasks.add(() -> {
+                final int[] rowC = new int[matrixSize];
+                for (int col = 0; col < matrixSize; col++) {
+                    final int[] rowA = matrixA[row];
+                    final int[] columnB = matrixBT[col];
+                    int sum = 0;
+                    for (int k = 0; k < matrixSize; k++) {
+                        sum += rowA[k] * columnB[k];
+                    }
+                    rowC[col] = sum;
+                }
+                matrixC[row] = rowC;
+                return null;
+            });
+        }
+        executor.invokeAll(tasks);
+        return matrixC;
+    }
+
+    public static int[][] concurrentMultiply(int[][] matrixA, int[][] matrixB, ExecutorService executor) throws InterruptedException {
+        final int matrixSize = matrixA.length;
+        final int[][] matrixC = new int[matrixSize][matrixSize];
+        final CountDownLatch latch = new CountDownLatch(matrixSize);
+
+        for (int row = 0; row < matrixSize; row++) {
+            final int[] rowA = matrixA[row];
+            final int[] rowC = matrixC[row];
+
+            executor.submit(() -> {
+                for (int idx = 0; idx < matrixSize; idx++) {
+                    final int elA = rowA[idx];
+                    final int[] rowB = matrixB[idx];
+                    for (int col = 0; col < matrixSize; col++) {
+                        rowC[col] += elA * rowB[col];
+                    }
+                }
+                latch.countDown();
+            });
+        }
+        latch.await();
         return matrixC;
     }
 
@@ -85,5 +142,15 @@ public class MatrixUtil {
             }
         }
         return true;
+    }
+
+    private static class ColumnMultipleResult {
+        private final int column;
+        private final int[] columnC;
+
+        public ColumnMultipleResult(int column, int[] columnC) {
+            this.column = column;
+            this.columnC = columnC;
+        }
     }
 }
